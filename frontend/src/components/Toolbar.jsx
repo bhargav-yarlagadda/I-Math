7ea45b659853377b9890d9e0e686d32075bc85fd
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-09-20 12:38:48 +0530

    fix/added eraser icons

--- frontend/src/components/Toolbar.jsx
+++ frontend/src/components/Toolbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SWATCHES } from '../constants/Constants';
-
-const Toolbar = ({ setBrushColor, brushColor, setBrushStroke, setReset, setIsEraser }) => {
+import { LuEraser } from "react-icons/lu";
+const Toolbar = ({ setBrushColor, brushColor,isEraser, setBrushStroke, setReset, setIsEraser }) => {
     return (
         <div className='flex flex-col md:flex-row text-white bg-gray-950 bg-opacity-90 p-1 backdrop-blur-md absolute top-5 w-[80vw] left-[10%] z-50'>
             <div className='w-full md:w-1/2 p-3 rounded-lg bg-gray-700 bg-opacity-20 justify-around flex flex-wrap gap-4 md:flex-row'>
@@ -23,12 +23,10 @@ const Toolbar = ({ setBrushColor, brushColor, setBrushStroke, setReset, setIsEra
                 }}>
                     Reset Canvas
                 </button>
-                <button className='bg-yellow-600 p-1 rounded-md hover:bg-yellow-700' onClick={() => {
+                <LuEraser className={` p-1 w-[50px] h-[40px] cursor-pointer rounded-md ${isEraser ?"bg-blue-700":""} hover:bg-yellow-700`} onClick={() => {
                     setIsEraser(true); // Enable eraser mode
                     setBrushColor("black"); // Set the stroke color to match the background
-                }}>
-                    Eraser
-                </button>
+                }}/>
                 <input type="range" min={1} max={32} defaultValue={2} onChange={(e) => {
                     setBrushStroke(e.target.value);
                 }} className='h-1 my-auto' />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,9 +8,10 @@ const Home = () => {
     const [reset, setReset] = useState(false);
     const [isEraser, setIsEraser] = useState(false); // State for eraser mode
 
+
     return (
         <>
-            <Toolbar setReset={setReset} setBrushStroke={setBrushStroke} setBrushColor={setBrushColor} brushColor={brushColor} setIsEraser={setIsEraser} />
+            <Toolbar isEraser={isEraser} setReset={setReset} setBrushStroke={setBrushStroke} setBrushColor={setBrushColor} brushColor={brushColor} setIsEraser={setIsEraser} />
             <Canvas reset={reset} setReset={setReset} brushStroke={brushStroke} brushColor={brushColor} isEraser={isEraser} />  
         </>
     );
